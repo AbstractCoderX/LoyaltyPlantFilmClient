638f3a2b03b38bfc7d563d7de3328fb418e82e3a
AbstractCoder <me@abstractcoder.ru>
2020-02-15 11:46:19 +0300

    Init

---
+++ src/main/java/ru/abstractcoder/filminfoclient/FilmInfoClientApplication.java
@@ -0,0 +1,13 @@
+package ru.abstractcoder.filminfoclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmInfoClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmInfoClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/abstractcoder/filminfoclient/FilmInfoClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.abstractcoder.filminfoclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmInfoClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
